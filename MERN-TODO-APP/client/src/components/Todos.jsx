13f753847400d149a2699c39969e7d7f6e7b8b00
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2025-04-01 19:11:04 +0200

    test

--- MERN-TODO-APP/client/src/components/Todos.jsx
+++ MERN-TODO-APP/client/src/components/Todos.jsx
@@ -238,7 +238,7 @@ const Todos = () => {
                   todo.isCompleted && "line-through text-[#63657b]"
                 }`}
               >
-                xxx {todo.title}
+                {todo.title}
               </span>
               <div className="px-3 flex gap-4">
                 <CheckCircle
