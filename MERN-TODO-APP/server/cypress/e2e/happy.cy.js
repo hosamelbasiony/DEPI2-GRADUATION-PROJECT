38451bff67eef5cb6043ae7bcad012d0a04563a0
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2025-04-01 17:11:38 +0200

    last e2e preparation

---
+++ MERN-TODO-APP/server/cypress/e2e/happy.cy.js
@@ -0,0 +1,226 @@
+/// <reference types="cypress" />
+
+const email = `depi2_${Date.now()}@egypt.eg`;
+
+describe('Authentication with proper creds', () => {
+  beforeEach(() => {
+    cy.wait(1000)
+  })
+
+  it('Register to the todo app with proper creds', () => {
+
+    cy.visit('/register')
+
+    cy.get('input[data-cy="email"]')
+      .should('exist')
+      .clear()
+      .type(email)
+
+    cy.get('input[data-cy="name"]')
+      .should('exist')
+      .type('Depi2 Cy User')
+
+    cy.get('input[data-cy="password"]')
+      .should('exist')
+      .clear()
+      .type('depi2')
+
+    cy.get('button[data-cy="register"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+  })
+
+
+  it('Login with proper creds and renders the landing page', () => {
+
+    cy.visit('/login')
+
+    cy.wait(1000)
+
+    cy.get('input[data-cy="email"]')
+      .should('exist')
+      .clear()
+      .type(email)
+
+    cy.get('input[data-cy="password"]')
+      .should('exist')
+      .clear()
+      .type('depi2')
+
+    cy.get('button[data-cy="login"]')
+      .should('exist')
+      .click()
+
+    cy.get('input[placeholder="Enter todo"]')
+      .should('exist')
+  })
+
+  it('Adds a todo', () => {
+
+    cy.visit('/login')
+
+    cy.get('input[data-cy="email"]')
+      .should('exist')
+      .clear()
+      .type(email)
+
+    cy.get('input[data-cy="password"]')
+      .should('exist')
+      .clear()
+      .type('depi2')
+
+    cy.get('button[data-cy="login"]')
+      .should('exist')
+      .click()
+
+    cy.get('input[placeholder="Enter todo"]')
+      .should('exist')
+      .type('Test Todo')
+      .should('have.value', 'Test Todo')
+
+    cy.get('button[data-cy="add-todo"]')
+      .should('exist')
+      .click()
+
+    cy.get('span[data-cy="todo-entry"]')
+      .should('have.length', 1)
+  })
+
+  it('Completes a todo', () => {
+
+    cy.visit('/login')
+
+    cy.get('input[data-cy="email"]')
+      .should('exist')
+      .clear()
+      .type(email)
+
+    cy.get('input[data-cy="password"]')
+      .should('exist')
+      .clear()
+      .type('depi2')
+
+    cy.get('button[data-cy="login"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+
+    cy.get('svg[data-cy="complete-todo"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+    cy.get('span[data-cy="todo-entry"]')
+      .should('satisfy', ($el) => {
+        const classList = Array.from($el[0].classList);
+        return classList.includes('line-through')
+      })
+  })
+
+  it('Cancel is-completed of a todo', () => {
+
+    cy.visit('/login')
+
+    cy.get('input[data-cy="email"]')
+      .should('exist')
+      .clear()
+      .type(email)
+
+    cy.get('input[data-cy="password"]')
+      .should('exist')
+      .clear()
+      .type('depi2')
+
+    cy.get('button[data-cy="login"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+
+    cy.get('svg[data-cy="complete-todo"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+    cy.get('span[data-cy="todo-entry"]')
+      .should('satisfy', ($el) => {
+        const classList = Array.from($el[0].classList);
+        return !classList.includes('line-through')
+      })
+  })
+
+  it('Updates a todo', () => {
+
+    cy.visit('/login')
+
+    cy.get('input[data-cy="email"]')
+      .should('exist')
+      .clear()
+      .type(email)
+
+    cy.get('input[data-cy="password"]')
+      .should('exist')
+      .clear()
+      .type('depi2')
+
+    cy.get('button[data-cy="login"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+
+    cy.get('svg[data-cy="edit-todo"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+
+    cy.get('input[data-cy="edit-todo"]')
+      .should('exist')
+      .clear()
+      .type('Test Todo Updated')
+
+    cy.get('button[data-cy="update-todo"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+
+    cy.get('span[data-cy="todo-entry"]').should(
+      "have.text",
+      "Test Todo Updated"
+    );
+  })
+
+  it('Deletes a todo', () => {
+
+    cy.visit('/login')
+
+    cy.get('input[data-cy="email"]')
+      .should('exist')
+      .clear()
+      .type(email)
+
+    cy.get('input[data-cy="password"]')
+      .should('exist')
+      .clear()
+      .type('depi2')
+
+    cy.get('button[data-cy="login"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+
+    cy.get('svg[data-cy="delete-todo"]')
+      .should('exist')
+      .click()
+
+    cy.wait(1000)
+    cy.get('span[data-cy="todo-entry"]')
+      .should('have.length', 0)
+  })
+})
